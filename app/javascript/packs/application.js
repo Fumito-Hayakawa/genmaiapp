530f277fdc925f77449815747345be60c0f2abdf
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-14 06:24:27 +0900

    yarn add bootstrap4 jquery3 pop実行、bootstrap導入作業成功

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// bootstrapv4導入のため追記
+import "bootstrap"
+import "../stylesheets/application"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
