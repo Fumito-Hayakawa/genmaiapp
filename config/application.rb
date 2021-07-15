076bf1711c7717c7934bac51d32aff1bc547a0b9
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-16 06:59:38 +0900

    deviseを日本語化設定

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module GenmaiApp
         routing_specs: false
     end
 
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
