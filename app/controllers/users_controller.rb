62da96616b0339fd8bad7a747830e67b30095824
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-17 22:15:53 +0900

    profileの編集、更新を作成。emailとpwは別画面、別コントローラで今後作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,14 @@ class UsersController < ApplicationController
   def index
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(params.require(:user).permit(:name, :introduction, :image_name))
-      flash[:notice] = "ユーザーIDが「#{@user.id}」の情報を更新しました"
+      flash[:notice] = "プロフィールの情報を更新しました"
       redirect_to root_url
     else
     render "edit"
