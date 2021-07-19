700c5ac0f230bedf08d2b8713d6c0ac0aec1964e
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-20 06:09:52 +0900

    ページタイトルを設定

--- spec/requests/users/profiles_request_spec.rb
+++ spec/requests/users/profiles_request_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "プロフィールページ", type: :request do
   let!(:user) { create(:user) }
 
-  xit "レスポンスが正常に表示されること" do
-    # 引数を2つ渡す必要があるが、書き方が分からずいったん放置。profileをcontroller直下とすることを検討中。"
-    get user_profile_path(user)
-    expect(response).to render_template('users/show')
+  it "レスポンスが正常に表示されること" do
+    get user_profiles_path(user.id)
+    expect(response).to be_successful
+    expect(response).to have_http_status "200"
   end
 end
