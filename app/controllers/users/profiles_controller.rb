7dad9906736ae1b2cd61112331dab05ef15521ab
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-22 06:07:52 +0900

    ログインしていなければ開けない画面を追加

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,4 +1,9 @@
 class Users::ProfilesController < ApplicationController
+  # ヘルパーメソッド読み込みのため
+  include ApplicationHelper
+  before_action :logged_in_user
+  before_action :correct_user,  only: [:edit, :update]
+  
   def show
     # はじめ@user = current_userとしていたが、テスト時にプロフィールへのリンクにあるuser_idがnilに。この通常通りの書き方が必要だった。
     @user = User.find(params[:user_id])
@@ -16,6 +21,16 @@ class Users::ProfilesController < ApplicationController
       redirect_to root_url
     else
       render "edit"
+    end
+  end
+
+  private
+
+  def correct_user
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      flash[:danger] = "このページへはアクセスできません"
+      redirect_to(root_url)
     end
   end
 end
