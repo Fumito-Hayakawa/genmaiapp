cbf5038d7a446f17dbade795a262ba28842b066f
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-22 13:10:07 +0900

    テストでリクエストスペックででログインできない問題を解決

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -1,7 +1,7 @@
 include ApplicationHelper # full_titleメソッドの読み込み
 
 def login_for_request(user)
-  post user_session_path, params: { session: { name: user.name,
+  post user_session_path, params: { user: { name: user.name,
                                         email: user.email,
                                         password: user.password } }
 end
