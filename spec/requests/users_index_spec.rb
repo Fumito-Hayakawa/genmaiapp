f9a1b1f13b6e1413afd1587b3cde2d3b83babf1a
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-24 07:19:46 +0900

    adminを変更できないことをテスト

--- spec/requests/users_index_spec.rb
+++ spec/requests/users_index_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "ユーザー一覧ページ", type: :request do
   let!(:user) { create(:user) }
+  let!(:admin_user) { create(:user, :admin) }
 
   context "ログインしたユーザーの場合" do
     it "レスポンスが正常に表示されること" do
@@ -17,5 +18,14 @@ RSpec.describe "ユーザー一覧ページ", type: :request do
       expect(response).to have_http_status "302"
       expect(response).to redirect_to new_user_session_path
     end
+  end
+
+  it "admin属性の変更が禁止されていること" do
+    login_for_request(user)
+    expect(user.admin).to be false
+    patch user_profiles_path(user), params: { user: { password: user.password,
+                                             password_confirmation: user.password,
+                                             admin: true } }
+    expect(user.reload.admin).to be false
   end
 end
