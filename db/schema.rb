1d7e8013e9856e3ad5c33ebd1dfc642e0d41078a
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-24 05:37:54 +0900

    マイグレーションでadminをデフォルトでfalseに変更。一度trueで誤ってマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_222137) do
+ActiveRecord::Schema.define(version: 2021_07_23_203643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_222137) do
     t.string "encrypted_password", default: "", null: false
     t.string "image_name"
     t.text "introduction"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
