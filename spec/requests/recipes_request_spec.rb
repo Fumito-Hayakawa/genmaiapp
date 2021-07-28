f1da3f4a90ed78cb2bbb6c72821c6a5e5412985f
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-07-28 15:07:16 +0900

    createのrequestspecを作成、通らず一旦スキップ

--- spec/requests/recipes_request_spec.rb
+++ spec/requests/recipes_request_spec.rb
@@ -5,10 +5,36 @@ RSpec.describe "レシピ登録", type: :request do
   let!(:recipe) { create(:recipe, user: user) }
 
   context "ログインしているユーザーの場合" do
-    it "レスポンスが正常に表示されること" do
+    before do
       login_for_request(user)
       get new_recipe_path
+    end
+
+    it "レスポンスが正常に表示されること" do
       expect(response).to have_http_status "200"
+      expect(response).to render_template('recipes/new')
+    end
+
+    xit "有効なレシピデータで登録できること" do
+      expect {
+        post recipes_path, params: { recipe: { name: "玄米握り",
+                                               description: "お手軽料理です",
+                                               portion: 1.5,
+                                               tips: "海苔をたっぷり巻きましょう",
+                                               episode: "何かを巻きたくて" } }
+      }.to change(Recipe, :count).by(1)
+      follow_redirect!
+      expect(response).to render_template('home/top')
+    end
+
+    xit "無効なレシピデータでは登録できないこと（名前なし）" do
+      expect {
+        post recipes_path, params: { recipe: { name: "",
+                                               description: "お手軽料理です",
+                                               portion: 1.5,
+                                               tips: "海苔をたっぷり巻きましょう",
+                                               episode: "何かを巻きたくて" } }
+      }.not_to change(Recipe, :count)
       expect(response).to render_template('recipes/new')
     end
   end
