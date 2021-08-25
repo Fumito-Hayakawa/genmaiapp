d42b49a15ffbb2a1eeb346ecc5a956f64ba454a4
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-08-26 07:24:03 +0900

    userモデルとrelationshipモデルの関連づけ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_one_attached :user_image
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
