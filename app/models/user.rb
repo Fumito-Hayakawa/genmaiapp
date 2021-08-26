5ba766420f8ab434433a44eab1285d1f94645296
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-08-27 07:36:46 +0900

    能動的なフォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :following, through: :active_relationships,  source: :followed
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -15,5 +16,17 @@ class User < ApplicationRecord
 
   def feed
     Recipe.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
