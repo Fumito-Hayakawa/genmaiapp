c5de1136bdecdfdfeecf4f10dd4c162aa47ce5db
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-08-27 07:47:46 +0900

    受動的なフォロー

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
                                   foreign_key: "follower_id",
                                   dependent: :destroy
   has_many :following, through: :active_relationships,  source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -28,5 +32,9 @@ class User < ApplicationRecord
 
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  def followed_by?(other_user)
+    followers.include?(other_user)
   end
 end
