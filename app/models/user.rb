e8cf6e7ea97ef90cda105749d97a73a0dec66596
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2021-09-25 12:50:39 +0900

    ステータスフィードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,10 @@ class User < ApplicationRecord
   validates :email, length: { maximum: 255 }
 
   def feed
-    Recipe.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Recipe.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
