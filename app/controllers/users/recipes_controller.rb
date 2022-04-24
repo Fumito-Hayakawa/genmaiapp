4dc30fc4e116a6e29e1813f882dfc7f7e93a86e9
早川史人 <hayakawafumito@hayakawafumitonoMacBook-Pro.local>
2022-04-25 06:42:45 +0900

    rubocop適用

--- app/controllers/users/recipes_controller.rb
+++ app/controllers/users/recipes_controller.rb
@@ -2,7 +2,7 @@ class Users::RecipesController < ApplicationController
   # ヘルパーメソッド読み込みのため
   include ApplicationHelper
   before_action :logged_in_user
-  
+
   def index
     @user = User.find(params[:user_id])
     @recipes = @user.recipes.page(params[:page]).per(8)
